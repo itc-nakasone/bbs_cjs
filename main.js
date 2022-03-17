e904a8a35c88dcf7f756426f955e64015c410917
nakasone <nakasone@std.it-college.ac.jp>
2022-03-17 11:25:12 +0900

    urlencodedのオプション変更(影響ないけど)

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(layouts);
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser("rzlqkCnvkNQnjguMvHuCfut6"));
 app.use(expressSession({
