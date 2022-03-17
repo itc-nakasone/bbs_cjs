72348274553169b9e7c6962b4ae229e73af32f7a
nakasone <nakasone@std.it-college.ac.jp>
2022-03-17 10:56:41 +0900

    ルート設定が意図通りに動かなかったので修正

--- routes/MessageRoutes.js
+++ routes/MessageRoutes.js
@@ -6,9 +6,10 @@ const {MessagesController} = require("../controllers/MessagesController");
 
 const router = Router();
 
-router.use("/new|delete/:tid",BbsMiddleware.requireLogin, BbsMiddleware.loadThread);
+router.use("/new/:tid",BbsMiddleware.requireLogin, BbsMiddleware.loadThread);
 router.get("/new/:tid", MessagesController.new);
 router.post("/new/:tid", MessagesController.create, BbsMiddleware.redirect);
+router.use("/delete/:tid/:mid",BbsMiddleware.requireLogin, BbsMiddleware.loadThread);
 router.get("/delete/:tid/:mid", MessagesController.delete, BbsMiddleware.redirect);
 
 module.exports = {
